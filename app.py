badc033b465d1573a0acf4eafeb426b14391ebcb
AMIRITHA A P <amiritha31@gmail.com>
2026-03-29 11:38:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 
-app=Flask(_name_)
+app=Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello from SheOps"
-app.run(host="0.0.0.0",port=5001)
+if __name__=="__main__":
+ app.run(host="0.0.0.0",port=5001)
